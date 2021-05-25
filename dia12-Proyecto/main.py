afd14612d29474de285f3638c1bd1788386bd05e
Andres <nastybitsz@gmail.com>
2021-05-24 23:51:11 -0400

    dia 13

--- dia12-Proyecto/main.py
+++ dia12-Proyecto/main.py
@@ -12,14 +12,11 @@ while intentos != 0:
     numero_user = int(input('Ingresa un numero para adivinar: '))
     if numero_pc > numero_user:
         print('FRIO no estas cerca, te falta para llegar del numero')
-        print(intentos)
         intentos -= 1
     elif numero_pc < numero_user:
         print('FRIO no estas cerca, te pasaste del numero')
-        print(intentos)
         intentos -= 1
     else:
         print('Lo lograste')
-        print(intentos)
         intentos = 0
 print('game over')
