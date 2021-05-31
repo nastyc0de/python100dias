b0a4f3684c043ae0dfa3ec20b98b4badc1ff4ed7
plague <plague@outlook.es>
2021-05-31 13:11:52 -0400

    snake primera parte

--- dia20/main.py
+++ dia20/main.py
@@ -1,20 +1,20 @@
-from turtle import Turtle, Screen
+from snake import Snake
+from turtle import Screen
 import time
 
 screen = Screen()
-screen.setup(600, 600)
+screen.setup(600, 600, 200)
 screen.bgcolor('black')
 screen.title('SNAKE')
 screen.tracer(0)
-start_position= [(0,0), (-20, 0),(-40, 0)]
-segments = []
 
-for position in start_position:
-    new_segment = Turtle(shape='square')
-    new_segment.penup()
-    new_segment.color('white')
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
 
 
@@ -22,10 +22,7 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for segment in range(len(segments)-1, 0, -1):
-        new_x = segments[segment -1].xcor()
-        new_y = segments[segment -1].ycor()
-        segments[segment].goto(new_x, new_y)
+    snake.move()
         
 
 screen.exitonclick()
