633799615b9072a271e1903b078920fe8f895878
Andres <nastybitsz@gmail.com>
2021-07-07 21:55:28 -0400

    primera parte finalizada refactorizar el codigo

--- dia57/blog-templating-start/main.py
+++ dia57/blog-templating-start/main.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
-@app.route('/post/<id>')
+    res = requests.get(url='https://jsonplaceholder.typicode.com/posts')
+    data = res.json()
+    return render_template("index.html", posts=data)
+@app.route('/post/<int:id>')
 def post(id):
-    print(id)
+    res = requests.get(url='https://jsonplaceholder.typicode.com/posts')
+    data = res.json()
+    info = data[id-1]
+    return render_template("post.html", post=info)
 if __name__ == "__main__":
     app.run(debug=True)
