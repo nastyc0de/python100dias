e3298cd1656de9b53afaed58c0831527aee217e0
xeneas <plague@outlook.es>
2021-07-26 11:17:30 -0400

    dia60

--- dia59/upgraded blog/app.py
+++ dia59/upgraded blog/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from api import posts
 
 app = Flask(__name__)
@@ -8,9 +8,26 @@ def index():
     return render_template('index.html', posts=posts)
 
 # contact
-@app.route('/contact')
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        print(f'{name},{email},{phone},{message}')
+        # msg = 'Datos enviado correctamente'
     return render_template('contact.html')
+# getting data
+# @app.route('/getData', methods=['POST', 'GET'])
+# def getData():
+
+#     name = request.form['name']
+#     email = request.form['email']
+#     phone = request.form['phone']
+#     message = request.form['message']
+#     print(f'{name},{email},{phone},{message}')
+#     return 'Datos enviados'
 # about
 @app.route('/about')
 def about():
