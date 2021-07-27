3c145f72ffeb8ba126b86116c431b0050e1b74a2
xeneas <plague@outlook.es>
2021-07-26 22:07:11 -0400

    dia61 corregido

--- dia61/form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class MyForm(FlaskForm):
-    email = StringField(label='Email:', validators=[DataRequired()])
-    password = PasswordField(label='Password:', validators=[DataRequired()])
-    submit = SubmitField(label='Log in')
--- dia61/main.py
+++ dia61/main.py
@@ -1,16 +1,24 @@
-from form import MyForm
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
+class Form(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(message='Este campo es obligatorio'), Email(message='Debes de agregar un @')])
+    password = PasswordField(label='Password', validators=[DataRequired(message='Este campo es obligatorio'), Length(8,message='Debes de agregar al menos 8 caracteres')])
+    submit = SubmitField(label='Log In')
+    
 app = Flask(__name__)
-app.secret_key = 'my_secret'
+app.secret_key = "some secret string"
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    form = MyForm()
+    form = Form()
     form.validate_on_submit()
     return render_template('login.html', form=form)
 
